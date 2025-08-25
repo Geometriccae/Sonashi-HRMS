0b947033bef6f88160727c8243a7e1e17ea4d6f8
Venkateshwaran <venkateshwaran2810@gmail.com>
2025-08-25 15:54:02 +0530

    url modified in login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,13 +13,13 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
-        username,
-        password
-      });
     // try {
-    //   const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, { username, password }, { withCredentials: true });
+    //   const res = await axios.post('http://localhost:5000/api/auth/login', {
+    //     username,
+    //     password
+    //   });
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, { username, password }, { withCredentials: true });
 
      // Store the token in localStorage
   localStorage.setItem("token", res.data.token);
